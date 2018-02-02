1bfef07616170cd4d87c86f3339343e936739e2f
Thomas Cheng <thomascheng1998@googlemail.com>
2018-02-02 12:01:31 +0000

    Added a print integer function, and integer division util

---
+++ math.s
@@ -0,0 +1,20 @@
+.global div_i
+
+@ Div a0 by a1, and return the result in a0 with the remainder in a1
+div_i:
+
+  mov r2, #0
+
+  div_i_loop:
+  subs r0, r1
+  bmi  div_i_end
+  add  r2, #1
+  b    div_i_loop
+
+  div_i_end:
+  add r0, r1
+  mov r1, r0
+  mov r0, r2
+
+  mov pc, lr
+
---
+++ print.s
@@ -0,0 +1,40 @@
+.global print_i
+
+@ Print an integer stored in r0 to UART0
+print_i:
+  push {r4, r5, r12, lr}
+
+  @ Reserve space for the number as a string - we're going to buffer the number
+  @ here then reverse
+  sub sp, #16
+
+  @ Pointer to the start of the buffer
+  mov r4, sp
+
+  @ Loop until r0 is 0, finding the remainder & printing
+  mov r5, #48 @ 48 is ascii for 0
+  ldr r12, =0x101f1000 @ UART0 port
+  print_i_buffer_loop:
+  @ Divide & get remainder in r1
+  mov r1, #10
+  bl div_i @ r0 / 10
+  add r1, r5
+
+  @ Buffer digit
+  str r1, [r4]
+
+  add r4, #1
+  cmp r0, #0
+  bne print_i_buffer_loop
+
+  @ Now print the whole buffer
+  print_i_loop:
+  ldr r0, [r4]
+  str r0, [r12]
+  sub r4, #1
+  cmp r4, sp
+  bge print_i_loop
+
+  add sp, #16
+
+  pop {r4, r5, r12, pc}
